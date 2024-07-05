d64d21f6bc54e1a770677ca1bf3be553fccbbb5f
kotfind <kotfind@yandex.ru>
2024-07-05 13:20:48 +0300

    Update tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,10 +2,10 @@ use assert_cmd::Command;
 use indoc::indoc;
 
 #[test]
-fn print_header() {
+fn basic() {
     Command::cargo_bin("tx")
         .unwrap()
-        .args([r#"2 1"#, "--print-header"])
+        .args(["2 1"])
         .write_stdin(indoc! {r#"
             A B C
             1 2 3
@@ -21,10 +21,10 @@ fn print_header() {
 }
 
 #[test]
-fn column_numbers_select() {
+fn named_columns() {
     Command::cargo_bin("tx")
         .unwrap()
-        .args([r#"2 1"#])
+        .args(["B A"])
         .write_stdin(indoc! {r#"
             A B C
             1 2 3
@@ -39,10 +39,29 @@ fn column_numbers_select() {
 }
 
 #[test]
-fn column_names_select() {
+fn print_header() {
     Command::cargo_bin("tx")
         .unwrap()
-        .args([r#"B A"#])
+        .args(["-h", "2 1"])
+        .write_stdin(indoc! {r#"
+            A B C
+            1 2 3
+            4 5 6
+        "#})
+        .assert()
+        .success()
+        .stdout(indoc! {r#"
+            B A
+            2 1
+            5 4
+        "#});
+}
+
+#[test]
+fn has_header_no_print() {
+    Command::cargo_bin("tx")
+        .unwrap()
+        .args(["-H", "2 1"])
         .write_stdin(indoc! {r#"
             A B C
             1 2 3
